b4496e999e075e2ac3eab18a9647df3cae9b2125
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-04-02 14:32:03 -0600

    El controlador y vistas de Equipos se creo

---
+++ FLJM2025042S12.AppWebMVC/Models/EquiposDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FLJM2025042S12.AppWebMVC.Models;
+
+public partial class EquiposDbContext : DbContext
+{
+    public EquiposDbContext()
+    {
+    }
+
+    public EquiposDbContext(DbContextOptions<EquiposDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Equipo> Equipos { get; set; }
+
+   
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Equipo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Equipos__3214EC07FCED34D9");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- FLJM2025042S12.AppWebMVC/Program.cs
+++ FLJM2025042S12.AppWebMVC/Program.cs
@@ -1,7 +1,16 @@
+using FLJM2025042S12.AppWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<EquiposDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
