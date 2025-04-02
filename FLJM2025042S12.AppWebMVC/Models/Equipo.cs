46860e16940b68afbb3312257c381d1e23fb5bc8
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-04-02 15:13:48 -0600

    Modifique las vistas de Euipos

--- FLJM2025042S12.AppWebMVC/Models/Equipo.cs
+++ FLJM2025042S12.AppWebMVC/Models/Equipo.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace FLJM2025042S12.AppWebMVC.Models;
 
 public partial class Equipo
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio")]
     public string Nombre { get; set; } = null!;
 
     public string? Descripcion { get; set; }
